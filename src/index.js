caed962f0fd617991e98e3f61868ff4e0d7c3972
philiparad <philip.arad@gmail.com>
2024-04-19 05:08:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+let root = document.getElementById('root');
+const rootElement = createRoot(root);
+rootElement.render(<App />);
