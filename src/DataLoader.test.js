71ecaeab4f62c57efb3d25a0056646e69f0ba720
Philip Arad <philip.arad@gmail.com>
2024-04-22 14:39:30 +0300

    Add files via upload

--- src/DataLoader.test.js
+++ src/DataLoader.test.js
@@ -1,40 +1,55 @@
-import { fireEvent, render, screen, waitFor, getByRole } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
-
+import React from 'react';
+import { fireEvent, render, screen, waitFor, getByRole, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import DataLoader from './DataLoader';
 
 jest.mock('fsm-react', () => ({
   useFSM: (initialState, states, transitions, transitionMap) => {
-	  let currentState = initialState;
-	  const setCurrentState = (nextState) => {
-		currentState = nextState;
-	  };
-	  const transition = (action) => {
-		const nextState = transitionMap[currentState][action];
-		if (nextState) {
-		  setCurrentState(nextState);
-		} else {
-		  setCurrentState(states.ERROR);
-		}
-	  };
-	  return [currentState, transition];
-	}
+    let currentState = initialState;
+    const setCurrentState = (nextState) => {
+      currentState = nextState;
+    };
+    const transition = (action) => {
+      let nextState = transitionMap[currentState][action];
+      if (nextState) {
+        setCurrentState(nextState);
+      } else {
+        setCurrentState(states.ERROR);
+      }
+    };
+    return [currentState, transition];
+  }
 }));
 
-describe('DataLoader Test', () => {
-  test('renders DataLoader component', async () => {
-    act(() => {
-      render(<DataLoader />);
-    });
-    expect(screen.getByText(/IDLE/i)).toBeInTheDocument;
-    const fetchButton = screen.getByText(/Fetch Data/i);
-    await fireEvent.click(fetchButton);
-    waitFor(() =>
+describe('DataLoader component', () => {
+  test('renders DataIdle and renders Loading state', async () => {
+    render(<DataLoader />);
+	waitFor(() =>
     {
-	expect(screen.getByText(/LOADING/i)).toBeInTheDocument;
-	jest.setTimeout(4000);
-	expect(screen.getByText(/SUCCESS/i)).toBeInTheDocument;
+    	expect(screen.getByText(/IDLE/i)).toBeInTheDocument();
+    	const fetchButton = screen.getByText('Fetch Data');
+    	fireEvent.click(fetchButton);
+		waitFor(() =>
+		{
+			expect(screen.getByText(/LOADING/i)).toBeInTheDocument();
+			waitFor(() => {
+				expect(screen.getByText(/SUCCESS/i)).toBeInTheDocument();
+			  }, { timeout: 12000 });
+		});
+    });
+  });
+
+  test('renders DeadEndPanel when transition to DEADEND state', async () => {
+    render(<DataLoader />);
+	waitFor(() =>
+	{
+        expect(screen.getByText(/IDLE/i)).toBeInTheDocument();
+        const deadEndButton = screen.getByText('Dead End');
+        fireEvent.click(deadEndButton);
+		waitFor(() =>
+		{
+			expect(screen.getByText(/DEADEND/i)).toBeInTheDocument();
+		});
     });
   });
 });
