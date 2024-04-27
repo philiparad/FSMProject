b329405e0b773d7e5d926f80bb4e08c6203b1106
Philip Arad <philip.arad@gmail.com>
2024-04-27 06:39:53 +0300

    Add files via upload

--- src/useFSM.js
+++ src/useFSM.js
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 
-export const useFSM = (initialState, states, transitions, transitionMap) => {
+export const useFSM = (initialState, states, transitions, transitionMap, transitionCallbacks) => {
   const [currentState, setCurrentState] = useState(initialState);
 
   const transition = (action) => {
     const nextState = transitionMap[currentState][action];
     if (nextState) {
       setCurrentState(nextState);
+
+	  //Notify potential end user, about the new state
+	  window.postMessage({ type: 'STATE', payload: {name:currentState} }, '*');
+
+      // Execute the callback function if it exists for this transition
+      if (transitionCallbacks && transitionCallbacks[action]) {
+        transitionCallbacks[action]();
+      }
     } else {
       setCurrentState(states.DEADEND);
     }
   };
-
   return [currentState, transition];
 };
